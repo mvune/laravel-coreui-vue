d14576352b0fbffd39c1c876259048ef695d3e1b
mvune <mvune01@gmail.com>
2019-03-06 16:29:59 +0100

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        @hasSection('title') @yield('title') | @endif
+        {{ config('app.name') }}
+    </title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset(mix('css/main.css')) }}">
+</head>
+
+<body>
+    <div id="app"></div>
+
+    <script defer src="{{ asset(mix('js/main.js')) }}"></script>
+</body>
+
+</html>
